fee7ec7bc01ad685b4059895eab4cd7e029aadc3
GrinchFromSMY <110434046+GrinchFromSMY@users.noreply.github.com>
2025-01-29 22:04:08 +0100

    Delete PWA-JS directory

--- PWA-JS/js/main.js
+++
@@ -1,9 +0,0 @@
-window.onload = () => {
-    'use strict';
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker
-            .register('./sw.js')
-            .then(() => console.log('Service Worker zarejestrowany pomyślnie.'))
-            .catch((err) => console.log('Błąd rejestracji Service Workera:', err));
-    }
-};
